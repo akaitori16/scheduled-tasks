46bbfbe211c648f7674220ec1b947b5a198fa12a
akaitori16 <soratori16@icloud.com>
2026-09-02 09:39:43 -0400

    Update main.py
    Still have no idea what I'm doing.

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("killedbycalculus@gmail.com")
+MY_PASSWORD = os.environ.get("unvw hiui kzyy kpbo")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
