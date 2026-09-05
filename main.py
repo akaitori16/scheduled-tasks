7a80fc83e7bf505ba41667f5c82fae2efaab9e4a
akaitori16 <soratori16@icloud.com>
2026-09-05 11:20:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
+import requests
+import os 
+
+api_key = os.environ.get(API_KEY)
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast?"
+bot_id = os.environ.get(BOT_ID)
+
+parameters = {
+    "lat": 33.9528,
+    "lon": -84.5499,
+    "appid": api_key,
+    "cnt": 4
+}
 
 
-from datetime import datetime
-import pandas
-import random
-import smtplib
-import os
+response = requests.get(url=OWM_Endpoint, params=parameters)
+response.raise_for_status()
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get(MY_EMAIL)
-MY_PASSWORD = os.environ.get(MY_PASSWORD)
+data = response.json()
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+def rain_expected():
+    for i in range(0,4):
+        cond_code = data["list"][i]["weather"][0]["id"]
+        if int(cond_code) < 700:
+            return True
+    return False
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+if rain_expected():
+    message = "It will rain today. It would be advisable to remain indoors."
+    url = f"https://api.telegram.org/{bot_id}/sendMessage?chat_id=8976361824&text={message}"
+    r = requests.get(url=url)
+    r.raise_for_status()
+    data = r.json()
+
+if not rain_expected():
+    message = "It will not rain today. Brainstorm excuses to not go anywhere."
+    url = f"https://api.telegram.org/{bot_id}/sendMessage?chat_id=8976361824&text={message}"
+    r = requests.get(url=url)
+    r.raise_for_status()
+    data = r.json()
+
+if str(data["ok"]).lower() == "true":
+    print("Message sent.")
